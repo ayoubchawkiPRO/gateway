10c10b00042c37fbee06b0339d3114c79ca4d263
ayoubchawkiPRO <ayoubchawkipros@gmail.com>
2023-02-19 23:23:20 +0100

    ajouter keycloack et dans pom

---
+++ src/main/java/org/sid/gateway/Security/KeycloakAdapterConfig.java
@@ -0,0 +1,12 @@
+package org.sid.gateway.Security;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakAdapterConfig {
+    @Bean
+    KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
